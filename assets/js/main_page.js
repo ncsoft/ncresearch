c84f77c3096ff299248009a305610e31c6807622
greentec <greentec@ncsoft.com>
2023-07-07 15:38:16 +0900

    remove logo change code

--- assets/js/main_page.js
+++ assets/js/main_page.js
@@ -5,23 +5,23 @@ function initEventListener() {
   
   addEventListener('scroll', (e) => {
     const main_div = document.getElementById('main');
-    const logo_blue = document.getElementById('logo_blue');
-    const logo_white = document.getElementById('logo_white');
+    // const logo_blue = document.getElementById('logo_blue');
+    // const logo_white = document.getElementById('logo_white');
     const site_header = document.getElementsByClassName('site-header')[0];
     const site_header_a = document.querySelectorAll('a.clear');
 
     if (window.scrollY <= 20) {
       removeClass(main_div, 'call-out_img_wheeled');
-      addClass(logo_blue, 'avatar_hide');
-      removeClass(logo_white, 'avatar_hide');
+    //   addClass(logo_blue, 'avatar_hide');
+    //   removeClass(logo_white, 'avatar_hide');
       removeClass(site_header, 'site_header_wheeled');
       site_header_a.forEach(c => addClass(c, 'main_page_a'));
     //   main_div.style.display = 'inline-block';
     }
     else {
       addClass(main_div, 'call-out_img_wheeled');
-      removeClass(logo_blue, 'avatar_hide');
-      addClass(logo_white, 'avatar_hide');
+    //   removeClass(logo_blue, 'avatar_hide');
+    //   addClass(logo_white, 'avatar_hide');
       addClass(site_header, 'site_header_wheeled');
       site_header_a.forEach(c => removeClass(c, 'main_page_a'));
     //   main_div.style.display = 'none';
